ccef6ef9db2fc406a16e6a2e622829c17067b017
Timon Gygax <timon.gygax@bsi-software.com>
2022-08-26 15:59:48 +0200

    Add scatter chart type to chart field

--- code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/services/lookup/ChartTypeLookupCall.java
+++ code/widgets/org.eclipse.scout.widgets.client/src/main/java/org/eclipse/scout/widgets/client/services/lookup/ChartTypeLookupCall.java
@@ -1,9 +1,9 @@
 /*
- * Copyright (c) 2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Distribution License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/org/documents/edl-v10.html
+ * https://www.eclipse.org/org/documents/edl-v10.html
  *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
@@ -30,6 +30,7 @@ public class ChartTypeLookupCall extends LocalLookupCall<String> {
     rows.add(new LookupRow<>(IChartType.BAR, "Bar"));
     rows.add(new LookupRow<>(IChartType.BAR_HORIZONTAL, "Bar Horizontal"));
     rows.add(new LookupRow<>(IChartType.BUBBLE, "Bubble"));
+    rows.add(new LookupRow<>(IChartType.SCATTER, "Scatter"));
     rows.add(new LookupRow<>(IChartType.COMBO_BAR_LINE, "Combo Bar Line"));
     rows.add(new LookupRow<>(IChartType.DOUGHNUT, "Doughnut"));
     rows.add(new LookupRow<>(IChartType.FULFILLMENT, "Fulfillment"));
